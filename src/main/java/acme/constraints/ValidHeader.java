3623c0e2b9b8b745ffce1cf05b7e774a59af57c9
celdorrui <celdorrui@alum.us.es>
2026-02-20 09:35:38 +0100

    feat: implemented ValidText and ValidHeader

--- src/main/java/acme/constraints/ValidHeader.java
+++ src/main/java/acme/constraints/ValidHeader.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -16,6 +17,7 @@ import org.hibernate.validator.constraints.Length;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
+@NotBlank
 @Length(min = 1, max = 75)
 public @interface ValidHeader {
 
