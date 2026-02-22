af2b6ff778d3b9b9b06cf6304dae5859727c771c
Ana Martin Carrillo <114395152+anamarva2@users.noreply.github.com>
2026-02-22 20:08:49 +0100

    feat:add constraints to model

---
+++ src/main/java/acme/entities/sponsorships/SponsorshipRepository.java
@@ -0,0 +1,22 @@
+
+package acme.entities.sponsorships;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface SponsorshipRepository extends AbstractRepository {
+
+	//CONSTRAINT Sponsorships cannot be published unless they have at least one donation.
+	@Query("SELECT SUM(d.money.amount) from Donation d where d.sponsorship.id = :id")
+	Double getSumTotalMoneyBySponsorship(@Param("id") int sponsorshipId);
+
+	@Query("SELECT d FROM Donation d WHERE d.sponsorship.id = :sponsorshipId")
+	List<Donation> findDonationBySponsorshipId(int sponsorshipId);
+
+}
