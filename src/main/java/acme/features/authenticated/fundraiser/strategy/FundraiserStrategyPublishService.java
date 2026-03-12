296056f57f2b724792f4156ce1ca92755f012f55
JuanCardesa <juacarsos@alum.us.es>
2026-03-12 18:47:47 +0100

    feat(fundraiser-strategy): add management and publish commands

---
+++ src/main/java/acme/features/authenticated/fundraiser/strategy/FundraiserStrategyPublishService.java
@@ -0,0 +1,69 @@
+package acme.features.authenticated.fundraiser.strategy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Authenticated;
+import acme.client.services.AbstractService;
+import acme.entities.strategies.Strategy;
+import acme.realms.Fundraiser;
+
+@Service
+public class FundraiserStrategyPublishService extends AbstractService<Authenticated, Strategy> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected FundraiserStrategyRepository	repository;
+
+	private Strategy						strategy;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void load() {
+		int id;
+		int userAccountId;
+
+		id = super.getRequest().getData("id", int.class);
+		userAccountId = super.getRequest().getPrincipal().getAccountId();
+		this.strategy = this.repository.findStrategyByIdAndFundraiserUserAccountId(id, userAccountId);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = super.getRequest().getPrincipal().hasRealmOfType(Fundraiser.class);
+		status = status && this.strategy != null && this.strategy.getDraftMode();
+
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
+	}
+
+	@Override
+	public void validate() {
+		boolean previousDraftMode;
+
+		previousDraftMode = this.strategy.getDraftMode();
+		this.strategy.setDraftMode(false);
+		super.validateObject(this.strategy);
+		this.strategy.setDraftMode(previousDraftMode);
+	}
+
+	@Override
+	public void execute() {
+		this.strategy.setDraftMode(false);
+		this.repository.save(this.strategy);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode", "monthsActive", "expectedPercentage");
+	}
+}
--- src/main/java/acme/features/authenticated/fundraiser/strategy/FundraiserStrategyRepository.java
+++ src/main/java/acme/features/authenticated/fundraiser/strategy/FundraiserStrategyRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
 import acme.entities.strategies.Strategy;
+import acme.entities.strategies.Tactic;
+import acme.realms.Fundraiser;
 
 @Repository
 public interface FundraiserStrategyRepository extends AbstractRepository {
@@ -16,4 +18,10 @@ public interface FundraiserStrategyRepository extends AbstractRepository {
 
 	@Query("select s from Strategy s where s.id = :id and s.fundraiser.userAccount.id = :userAccountId")
 	Strategy findStrategyByIdAndFundraiserUserAccountId(int id, int userAccountId);
+
+	@Query("select f from Fundraiser f where f.userAccount.id = :userAccountId")
+	Fundraiser findFundraiserByUserAccountId(int userAccountId);
+
+	@Query("select t from Tactic t where t.strategy.id = :strategyId")
+	Collection<Tactic> findTacticsByStrategyId(int strategyId);
 }
