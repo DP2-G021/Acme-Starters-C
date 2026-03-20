ccb32de7e07fd7a30ada5c305980f18fad7435de
JuanCardesa <juacarsos@alum.us.es>
2026-03-20 11:19:05 +0100

    feat: enhance draft mode display in strategy services and views

--- src/main/java/acme/features/authenticated/fundraiser/strategy/FundraiserStrategyUpdateService.java
+++ src/main/java/acme/features/authenticated/fundraiser/strategy/FundraiserStrategyUpdateService.java
@@ -3,6 +3,7 @@ package acme.features.authenticated.fundraiser.strategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.client.components.models.Tuple;
 import acme.client.components.principals.Authenticated;
 import acme.client.services.AbstractService;
 import acme.entities.strategies.Strategy;
@@ -58,6 +59,9 @@ public class FundraiserStrategyUpdateService extends AbstractService<Authenticat
 
 	@Override
 	public void unbind() {
-		super.unbindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode", "monthsActive", "expectedPercentage");
+		Tuple tuple;
+
+		tuple = super.unbindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode", "monthsActive", "expectedPercentage");
+		tuple.put("draftModeDisplay", FundraiserStrategyI18nHelper.draftModeDisplay(this.strategy.getDraftMode()));
 	}
 }
